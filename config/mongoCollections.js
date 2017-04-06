a4b57613198600a80f3d58d1fa436c7900275b93
Matt Telker <mtelker@stevens.edu>
2017-04-06 15:05:35 -0400

    mongo connection files added

---
+++ config/mongoCollections.js
@@ -0,0 +1,22 @@
+const dbConnection = require("./mongoConnection");
+
+
+let getCollectionFn = (collection) => {
+    let _col = undefined;
+
+    return () => {
+        if (!_col) {
+            _col = dbConnection().then(db => {
+                return db.collection(collection);
+            });
+        }
+
+        return _col;
+    }
+}
+
+/* Now, you can list your collections here: */
+module.exports = {
+    pets: getCollectionFn("pets"),
+    users: getCollectionFn("users")
+};
