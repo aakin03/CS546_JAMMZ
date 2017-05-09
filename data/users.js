ad8e704a309459df8b011d5238da77a8915f837b
Monica Razak <Monica Razak>
2017-05-09 15:15:16 -0400

    Added deletion and update methods

--- data/users.js
+++ data/users.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcrypt-nodejs");
 const users = mongoCollections.users;
 
 let exportedMethods = {
+	
     addUser(userName, password, name, age) {
         if (!userName)
             return Promise.reject("You must supply a username.");
@@ -42,16 +43,14 @@ let exportedMethods = {
                     return this.getUser(newId)
                 });
         });
-
-
-
     },
+	
     getUser(id) {
         if (!id)
             return Promise.reject("You must provide an id to search for");
 
-        return users().then((usersCollection) => {
-            return usersCollection.findOne({ _id: id });
+        return users().then((userCollection) => {
+            return userCollection.findOne({ _id: id });
         })
             .then((result) => {
                 if (result === null) {
@@ -61,11 +60,35 @@ let exportedMethods = {
                     return result;
                 }
             })
-    }
+    },
+	
+	updateUser(id, updatedUser) {
+        return this.getUserById(id).then((currentUser) => {
+            let updatedUser = {
+                name: updatedUser.name,
+                age: updatedUser.age
+            };
 
+            let updateCommand = { 
+                $set: updatedUser
+            };
 
-    //deleteUser
-    //updateUser
+            return userCollection.updateOne({ _id: id }, updateCommand).then(() => {
+                return this.getUserById(id);
+            });
+        });
+    },
+	
+    removeUser(id) {
+        return users().then((userCollection) => {
+            return userCollection.removeOne({ _id: id }).then((deletionInfo) => {
+                if (deletionInfo.deletedCount === 0) {
+                    throw (`Could not delete user with id of ${id}`)
+                }
+            });
+        });
+    },
+	
 };
 
 module.exports = exportedMethods;
