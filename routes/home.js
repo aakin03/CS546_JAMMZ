3ab651627f42e93f78951906c60ff4097470a9e4
Ayse Akin <Ayse Akin>
2017-05-11 19:35:50 -0400

    UserProfile Updated with Pet Info

--- routes/home.js
+++ routes/home.js
@@ -107,7 +107,8 @@ router.get("/home", function (req, res) {
                 info = "Successfully enrolled a new pet!"
             }
             let petsArray = result;
-            res.render("layouts/home.handlebars", { user: req.user.userName, id: req.user._id, name: req.user.profile.name, age: req.user.profile.age, wishlist: req.user.profile.wishlist, petsArry: petsArray});
+        console.log(petsArray);
+            res.render("layouts/home.handlebars", { user: req.user.userName, id: req.user._id, name: req.user.profile.name, age: req.user.profile.age, wishlist: req.user.profile.wishlist, petsArray: petsArray});
         })
         .catch((e) => {
             res.render("layouts/home", { user: req.user.userName, info: info, petsArray: e });
